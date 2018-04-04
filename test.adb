cbace1b7c5ecfadcb8ab56e3117e9f82e4674f4d
reivax-boucoi <reivax.boucoi@laposte.net>
2018-04-04 21:18:05 +0200

    Update test.adb

--- test.adb
+++ test.adb
@@ -1,13 +1,30 @@
 --test raw
-procedure drawSnake(S:P_Element)is
+procedure draw(T:T_Table)is
 begin
-  if(S/=null)then
-    
-  end if;
-end drawSnake;
+-- for (b in T'Range)loop
+-- draw every block in table
+-- end loop;
+end draw;
+
+procedure move(S:P_Element,T:T_Table,Dir:T_Direction)is
+  Head:P_Element;
+begin
+  Case Dir is
+    when Nord =>
+      Head := S.Serpent.y-1;
+    when Sud =>
+      Head := S.Serpent.y+1;
+    when Est =>
+      Head := S.Serpent.x+1;    
+    when Ouest =>
+      Head := S.Serpent.x-1;    
+    when Immobile => return;
+    when others => --help !!!
+  Head.all.Serpent
+end move;
 
 Lost:boolean:=false;
-T:T_Table;
+Screen:T_Table;
 Start:P_Element;
 
 S:T_Snake;
@@ -16,8 +33,9 @@ InitialiserJeu(T;Start.all.Serpent);
 
 while(lost=false) loop
   --do game loop here
-  drawSnake(Start);
-  if(false)then
+  move(S,Screen,DetecterDirection);
+  draw(Screen);
+  if(false)then--checkLost(Start)
     lost:=true;
   end if;
 end loop;
