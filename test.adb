a3e82783bccbd084461a7cf51acdd328e5215ef8
reivax-boucoi <reivax.boucoi@laposte.net>
2018-04-04 21:03:19 +0200

    Update test.adb

--- test.adb
+++ test.adb
@@ -1,5 +1,12 @@
 --test raw
+procedure drawSnake(S:P_Element)is
+begin
+  if(S/=null)then
+    
+  end if;
+end drawSnake;
 
+Lost:boolean:=false;
 T:T_Table;
 Start:P_Element;
 
@@ -7,3 +14,12 @@ S:T_Snake;
 InitialiserCarte;
 InitialiserJeu(T;Start.all.Serpent);
 
+while(lost=false) loop
+  --do game loop here
+  drawSnake(Start);
+  if(false)then
+    lost:=true;
+  end if;
+end loop;
+
+--free memory here
